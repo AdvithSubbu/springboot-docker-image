0f3e1307b3cfd9ecc69ff8bee7ac7532bb0e71f9
Kota <kiran.kota@linquest.com>
2020-02-17 14:30:09 -0700

    Added 2

--- springboot-dockerimage/src/main/java/se/ivankrizsan/spring/controllers/HelloController.java
+++ springboot-dockerimage/src/main/java/se/ivankrizsan/spring/controllers/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
         final Model inModel) {
         final StringBuilder theMessageBuilder = new StringBuilder();
         theMessageBuilder
-            .append("Hello ")
+            .append("Hello2 ")
             .append(inName)
             .append(", the time is now ")
             .append(new Date().toString())
